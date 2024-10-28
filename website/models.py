d1a1ba2440752906fd4b565d5c3f52fece129b59
cytronVEVO <maldonadoalex44@gmail.com>
2024-10-29 03:46:31 +1000

    Fixed mistake in models.py
    Changed .integer to .string, works now

--- website/models.py
+++ website/models.py
@@ -34,7 +34,7 @@ class Event(db.Model):
     event_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    Description = db.Column(db.Integer(250), nullable=False)
+    Description = db.Column(db.String(250), nullable=False)
     Image = db.Column(db.String(60), nullable=False, default='default.jpg')
     Start_time = db.Column(db.Time, nullable=False)
     date = db.Column(db.Date, nullable=False)
